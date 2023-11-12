eada24511dc667c8af808ba41ba5d06f818ded83
Javier Ferrer <javier.f.g@um.es>
2023-11-12 18:38:59 +0100

    Improve itemList

--- src/templates/itemList.js
+++ src/templates/itemList.js
@@ -5,11 +5,11 @@ import '../styles/itemlist.scss';
 import Title from '../components/title';
 
 export default function ItemList({ caption, data }) {
-  const { items } = data.nodes[0].frontmatter;
+  const { title, items } = data.nodes[0].frontmatter;
 
   return (
     <section className="itemlist-section">
-      <Title caption={caption} />
+      <Title caption={caption || title} />
       <article>
         <ul>
           {items.map((item, i) => (
